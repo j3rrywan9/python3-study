7fa745c46d648d42ca4fe71a8ef867a1f457ea45
Jerry Wang <j3rry.wan9@gmail.com>
2020-05-23 22:32:46 -0700

    add Longest Substring Without Repeating Characters

---
+++ algorithms/leetcode/longest_substring_without_repeating_characters.py
@@ -0,0 +1,29 @@
+# LC 3
+
+
+class Solution(object):
+    def length_of_longest_substring(self, s):
+        """
+        :param s: input string
+        :type s: str
+        :return: the length of the longest substring without repeating characters
+        :rtype: int
+        """
+        start = max_length = 0
+        used_char = {}
+
+        for index, char in enumerate(s):
+            if char in used_char and start <= used_char[char]:
+                start = used_char[char] + 1
+            else:
+                max_length = max(max_length, index - start + 1)
+
+            used_char[char] = index
+
+        return max_length
+
+
+if __name__ == '__main__':
+    print(Solution().length_of_longest_substring('abcabcbb'))
+    print(Solution().length_of_longest_substring('bbbbb'))
+    print(Solution().length_of_longest_substring('pwwkew'))
