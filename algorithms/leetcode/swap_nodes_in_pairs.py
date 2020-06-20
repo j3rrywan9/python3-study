c0bb359946da6e9feca3602a1ae0a7df79adcd7b
Jerry Wang <j3rry.wan9@gmail.com>
2020-06-20 16:29:50 -0700

    add Swap Nodes in Pairs

---
+++ algorithms/leetcode/swap_nodes_in_pairs.py
@@ -0,0 +1,32 @@
+# LC 24
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swap_pairs(self, head):
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        prev_node = dummy
+
+        while head and head.next:
+            # Nodes to be swapped
+            first_node = head
+            second_node = head.next
+
+            # Swapping
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            # Reinitializing the head and prev_node for next swap
+            prev_node = first_node
+            head = first_node.next
+
+        # Return the new head node
+        return dummy.next
