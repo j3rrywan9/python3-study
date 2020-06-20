b50ecb3147be7f45950c278487485f0150a1113f
Jerry Wang <j3rry.wan9@gmail.com>
2020-06-20 16:19:54 -0700

    update Two Sum

--- algorithms/leetcode/two_sum.py
+++ algorithms/leetcode/two_sum.py
@@ -3,18 +3,14 @@
 
 class Solution(object):
     def two_sum(self, nums, target):
-        map = {}
+        d = {}
 
         for index, num in enumerate(nums):
             complement = target - num
 
-            if complement in map:
-                return [map[complement], index]
+            if complement in d:
+                return [d[complement], index]
             else:
-                map[num] = index
+                d[num] = index
 
         raise RuntimeError("No two sum solution")
-
-
-if __name__ == '__main__':
-    print(Solution().two_sum([2, 7, 11, 15], 9))
