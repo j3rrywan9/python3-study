3efa9e8e94612da1e3ecdd1d7b92c12363b68014
Jerry Wang <j3rry.wan9@gmail.com>
2020-07-28 11:39:14 -0700

    add Bulb Switcher

---
+++ algorithms/leetcode/bulb_switcher.py
@@ -0,0 +1,7 @@
+# LC 319
+import math
+
+
+class Solution:
+    def bulb_switch(self, n):
+        return int(math.sqrt(n))
---
+++ algorithms/leetcode/tests/test_bulb_switcher.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from ..bulb_switcher import Solution
+
+
+def test_example1():
+    assert Solution().bulb_switch(3) == 1
