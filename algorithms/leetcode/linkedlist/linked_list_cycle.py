267b9bf1be1eb4a4b1ddd1cc5997f063015aa019
Jerry Wang <j3rry.wan9@gmail.com>
2020-08-09 22:48:30 -0700

    add Linked List Cycle

---
+++ algorithms/leetcode/linkedlist/linked_list_cycle.py
@@ -0,0 +1,19 @@
+# LC 141
+
+
+class Solution:
+    def has_cycle(self, head):
+        if head is None or head.next is None:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+
+            slow = slow.next
+            fast = fast.next.next
+
+        return True
